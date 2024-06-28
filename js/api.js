d94aaf9f8e1d36cf05d89aaa3cde98f643462347
Francisco Silva <133883562+vXkn6@users.noreply.github.com>
2024-06-27 21:14:17 -0400

    Add files via upload

--- js/api.js
+++ js/api.js
@@ -1,38 +1,42 @@
-const url = 'https://api.jikan.moe/v4/top/manga';
 
-function makeCard (character){
-    
-    const {title,synopsis} = character;
-    const contenedor = document.querySelector(".contenedor");
-
-    const titulo = document.createElement("h5");
-    titulo.textContent = title;
-
-    const portada = document.createElement("img");
-    portada.src = character.images.jpg.image_url;
-
-    const resumen = document.createElement("p");
-    resumen.textContent= synopsis;
-
-    const Card = document.createElement("div");
-    Card.appendChild(titulo);
-    Card.appendChild(portada);
-    Card.appendChild(resumen);
-
-    contenedor.appendChild(Card);
-} 
-
-async function getManga(){
+const cargarMangas = async () => {
     try {
-        const response = await fetch(url);
-        const {data} = await response.json();
+        const respuesta = await fetch(`https://api.jikan.moe/v4/top/manga`);
 
-        for(let i = 0; i< data.length; i++){
-            makeCard(data[i]);
+        console.log(respuesta);
+
+        // Si la respuesta es correcta
+        if (respuesta.status === 200) {
+            const datos = await respuesta.json();
+
+            let mangas = '';
+            datos.data.forEach(manga => {
+                mangas += `
+			<div class="contenedor-items">
+                <div class="item">
+                <span class="titulo-item">${manga.title}</span>
+                <img src="${manga.images.jpg.image_url}" alt="" class="img-item">
+                <h3 class="precio-item">$15.000</h3>
+                <button class="boton-item">Agregar al Carrito</button>
+                </div>
+            </div>
+				`;
+            });
+
+            document.getElementById('mangaList').innerHTML = mangas;
+
+        } else if (respuesta.status === 401) {
+            console.log('Pusiste la llave mal');
+        } else if (respuesta.status === 404) {
+            console.log('El manga que buscas no existe');
+        } else {
+            console.log('Hubo un error y no sabemos que paso');
         }
+
     } catch (error) {
-        console.error(error);
+        console.log(error);
     }
 
 }
-getManga();
+
+cargarMangas();
