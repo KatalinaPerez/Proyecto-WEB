0efefe54a1f14cafaa2690f5a2b5b60c84812a08
KatalinaPerez <133880491+KatalinaPerez@users.noreply.github.com>
2024-07-01 01:23:05 -0400

    Entrega 3

---
+++ Library_app/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path
+from . import views
+from .views import index, libros, comics,cart, adminAdd, eliminar_del_carrito, registro, adminList, adminUp, pagar,mangas, adminDelete
+
+urlpatterns = [
+    path('', index, name="index"),
+    path('libros/', libros, name="libros"),
+    #path('busqueda_resultados/', views.busqueda_resultados, name="busqueda_resultados"),
+    path('comics/', comics, name="comics"),
+    path('cart/', cart, name="cart"),
+    path('adminAdd/', adminAdd, name="adminAdd"),
+    path('agregar_al_carrito/<int:producto_id>/', views.agregar_al_carrito, name='agregar_al_carrito'),
+    path('cart/eliminar/<int:producto_id>/', eliminar_del_carrito, name="eliminar_del_carrito"),
+    path('registro/', registro, name="registro"),
+    path('adminList/', adminList, name="adminList"),
+    path('adminUp/<id>/', adminUp, name="adminUp"),
+    path('cart/pagar/', views.pagar, name='pagar'),
+    path('get-time/', views.get_time, name='get_time'),
+    path('mangas/', mangas, name="mangas"),
+    path('adminDelete/<id>/', adminDelete, name="adminDelete"),
+]
